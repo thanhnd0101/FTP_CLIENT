2b48d96980e57a9dee640d2efa27173a21b07f5e
Nguyen Duy Thanh <thanhnguyenduy2304@gmail.com>
2018-05-19 22:58:30 +0700

    add exists_in_arr, recv_reply, mkdir, rmdir

--- ftp_client-master/ftp_client/client.cpp
+++ ftp_client-master/ftp_client/client.cpp
@@ -70,13 +70,12 @@ int main(int argc, char *argv[])
 	// Login
 	status = ftp_login(connect_SOCKET);
 	//error
-	if (status == -1){
-		printf("Can not login!!");
+	if (status == FTP_FAIL){
 		closesocket(connect_SOCKET);
 		return 0;
 	}
 	pwd(connect_SOCKET, "PWD\r\n");
-	TYPE_I(connect_SOCKET, "TYPE I\r\n");
+	TYPE(connect_SOCKET, "TYPE I\r\n");
 
 	command *cmdstruct=new command;
 	//test passive mode
@@ -85,7 +84,7 @@ int main(int argc, char *argv[])
 	memset(whole_cmd, '\0', BUFLEN);
 
 	while (1){
-		if (read_ftpclien_cmd(whole_cmd, cmdstruct) == -1){
+		if (read_ftpclien_cmd(whole_cmd, cmdstruct) == FTP_FAIL){
 			printf("Ivalid Commmand");
 			continue;
 		}
@@ -98,7 +97,6 @@ int main(int argc, char *argv[])
 			else if (strcmp(cmdstruct->_cmd, "STOR") == 0){
 				put(connect_SOCKET, host,whole_cmd, cmdstruct);
 				continue;
-				
 			}
 			else if (strcmp(cmdstruct->_cmd, "RETR") == 0){
 				get(connect_SOCKET,host, whole_cmd, cmdstruct);
@@ -127,6 +125,18 @@ int main(int argc, char *argv[])
 				//cmdstruct->pasv_mode = false;
 				continue;
 			}
+			else if (strcmp(cmdstruct->_cmd, "MKD") == 0){
+				mkdir(connect_SOCKET, whole_cmd);
+				continue;
+			}
+			else if (strcmp(cmdstruct->_cmd, "RMD") == 0){
+				rmdir(connect_SOCKET, whole_cmd);
+				continue;
+			}
+			else if (strcmp(cmdstruct->_cmd, "PWD") == 0){
+				pwd(connect_SOCKET, whole_cmd);
+				continue;
+			}
 			else{
 				break;
 			}
@@ -149,8 +159,9 @@ int ftp_login(SOCKET &connect_SOCKET)
 	// Check connection
 	while ((len_temp = recv(connect_SOCKET, buf, BUFLEN, 0)) > 0) {
 		char *p_end = NULL;
+		buf[len_temp] = '\0';
+		printf("%s", buf);
 		reply_code = strtol(buf, &p_end, 10);
-		print_reply_code(reply_code);
 
 		// 220 Service ready for new user
 		if (reply_code == 220) {
@@ -177,33 +188,26 @@ int ftp_login(SOCKET &connect_SOCKET)
 	memset(buf, 0, BUFLEN);
 
 	// Check server recieving username
-	while ((len_temp = recv(connect_SOCKET, buf, BUFLEN, 0)) > 0) {
-		char *p_end = NULL;
-		reply_code = strtol(buf, &p_end, 10);
-		print_reply_code(reply_code);
+	vector<int> arr_expect_user = { 230, 331 };
+	status = recv_reply(connect_SOCKET, arr_expect_user);
+	if (status==FTP_FAIL)
+		return FTP_FAIL;
 
-		// 230 User logged in, proceed
-		// 331 User name okay, need password
-		if (reply_code == 230 || reply_code == 331) {
-			break;
-		}
-		// error happen
-		return -1;
-	}
-	memset(buf, 0, BUFLEN);
-
-	// If logged in, exit
-	if (reply_code == 230) {
-		return 0;
-	}
-
-	// username looks good, need password
 	// Get password
 	// PASS <SP> <password> <CRLF>
 	printf("Password: ");
 	char password[BUFLEN - 10];
-	fgets(password, BUFLEN - 10, stdin);
-	password[strcspn(password, "\n")] = 0; // remove trailing '\n'
+	
+	//hide password input
+	int temp_i = 0, temp_ch;
+	while ((temp_ch = _getch()) != Enter) {
+		printf("*");
+		password[temp_i++] = temp_ch;
+	}
+	password[temp_i] = '\0';
+	fflush(stdin);
+	printf("\n");
+
 	sprintf_s(buf, "PASS %s\r\n", password);
 
 	// Send password
@@ -214,139 +218,10 @@ int ftp_login(SOCKET &connect_SOCKET)
 	memset(buf, 0, BUFLEN);
 
 	// Check server recieving password
-	while ((len_temp = recv(connect_SOCKET, buf, BUFLEN, 0)) > 0) {
-		char *p_end = NULL;
-		reply_code = strtol(buf, &p_end, 10);
-		print_reply_code(reply_code);
-
-		// 230 User logged in, proceed
-		if (reply_code == 230) {
-			break;
-		}
-		// error happen
-		return -1;
-	}
-	memset(buf, 0, BUFLEN);
-
-	return 0;
-}
-
-void print_reply_code(int reply_code)
-{
-	if (reply_code == 110)
-		printf(
-		    "%d Restart marker reply.\n"
-		    "    In this case, the text is exact and not left to the\n"
-		    "    particular implementation; it must read :\n"
-		    "    \tMARK yyyy = mmmm\n"
-		    "    Where yyyy is User - process data stream marker, and "
-		    "mmmm\n"
-		    "    server's equivalent marker (note the spaces between "
-		    "markers\n"
-		    "    and \"=\").\n",
-		    reply_code);
-
-	else if (reply_code == 120)
-		printf("%d Service ready in nnn minutes.\n", reply_code);
-	else if (reply_code == 125)
-		printf("%d Data connection already open; transfer starting.\n",
-		       reply_code);
-	else if (reply_code == 150)
-		printf("%d File status okay; about to open data connection.\n",
-		       reply_code);
-	else if (reply_code == 200)
-		printf("%d Command okay.\n", reply_code);
-	else if (reply_code == 202)
-		printf(
-		    "%d Command not implemented, superfluous at this site.\n",
-		    reply_code);
-	else if (reply_code == 211)
-		printf("%d System status, or system help reply.\n", reply_code);
-	else if (reply_code == 212)
-		printf("%d Directory status.\n", reply_code);
-	else if (reply_code == 213)
-		printf("%d File status.\n", reply_code);
-	else if (reply_code == 214)
-		printf("%d Help message.\n"
-		       "    On how to use the server or the meaning of a "
-		       "particular\n"
-		       "    non - standard command.This reply is useful only "
-		       "to the\n"
-		       "    human user.\n",
-		       reply_code);
-	else if (reply_code == 215)
-		printf("%d NAME system type.\n"
-		       "    Where NAME is an official system name from the "
-		       "list in the\n"
-		       "    Assigned Numbers document.\n",
-		       reply_code);
-	else if (reply_code == 220)
-		printf("%d Service ready for new user.\n", reply_code);
-	else if (reply_code == 221)
-		;
-	else if (reply_code == 225)
-		;
-	else if (reply_code == 226)
-		;
-	else if (reply_code == 227)
-		;
-	else if (reply_code == 230)
-		printf("%d User logged in, proceed.\n", reply_code);
-	else if (reply_code == 250)
-		printf("%d Requested file action okay, completed.\n",reply_code);
-	else if (reply_code == 257)
-		printf("%d 'PATHNAME' created.",reply_code);
-	else if (reply_code == 331)
-		printf("%d User name okay, need password.\n", reply_code);
-	else if (reply_code == 332)
-		printf("%d Need account for login.\n", reply_code);
-	else if (reply_code == 350)
-		;
-	else if (reply_code == 421)
-		printf("%d Service not available, closing control connection.\n"
-		       "    This may be a reply to any command if the service "
-		       "knows it\n"
-		       "    must shut down.\n",
-		       reply_code);
-	else if (reply_code == 425)
-		;
-	else if (reply_code == 426)
-		;
-	else if (reply_code == 450)
-		;
-	else if (reply_code == 451)
-		;
-	else if (reply_code == 451)
-		;
-	else if (reply_code == 500)
-		printf("%d Syntax error, command unrecognized.\n"
-		       "    This may include errors such as command line too "
-		       "long.\n",
-		       reply_code);
-	else if (reply_code == 500){
-		printf("%d Syntax error, command unrecognized.\nThis may include errors such as command line too long.", reply_code);
-	}
-	else if (reply_code == 501)
-		printf("%d Syntax error in parameters or arguments.\n",
-		       reply_code);
-	else if (reply_code == 502){}
-		
-	else if (reply_code == 503)
-	{ }
-	else if (reply_code == 504)
-	{ }
-	else if (reply_code == 530)
-		printf("%d Not logged in.\n", reply_code);
-	else if (reply_code == 532)
-	{ }
-	else if (reply_code == 550)
-	{ }
-	else if (reply_code == 551)
-	{ }
-	else if (reply_code == 552)
-	{ }
-	else if (reply_code == 553)
-	{ }
+	vector<int> arr_expect_pass = { 230 };
+	status = recv_reply(connect_SOCKET, arr_expect_pass);
+	
+	return status;
 }
 
 int read_ftpclien_cmd(char* buff,command *cmdstruct){
@@ -418,7 +293,7 @@ int read_ftpclien_cmd(char* buff,command *cmdstruct){
 	}
 	else
 	{
-		return -1;
+		return FTP_FAIL;
 	}
 
 	memset(buff, '\0', BUFLEN);
@@ -428,20 +303,7 @@ int read_ftpclien_cmd(char* buff,command *cmdstruct){
 		strncat(buff, cmdstruct->_args, sizeof(cmdstruct->_args));
 	}
 	strcat(buff, "\r\n\0");
-	return 0;
-}
-
-int feedback(SOCKET &connect_SOCKET,char* feedback,int size){
-	memset(feedback, '\0', size);
-
-	int status = recv(connect_SOCKET, feedback, size, 0);
-
-
-	if (status < 0){
-		printf("recv() error %d\n", WSAGetLastError());
-		return -1;
-	}
-	return 0;
+	return FTP_WIN;
 }
 
 SOCKET pasv(SOCKET &connect_SOCKET, SOCKET &DataSocket, char* host){
@@ -454,14 +316,14 @@ SOCKET pasv(SOCKET &connect_SOCKET, SOCKET &DataSocket, char* host){
 	int status = send(connect_SOCKET, pasv, strlen(pasv), 0);
 	if ( status==SOCKET_ERROR){
 		printf("send() error %d\n", WSAGetLastError());
-		return -1;
+		return FTP_FAIL;
 	}
 
 	char temp[BUFLEN];
 	memset(temp, '\0', BUFLEN);
 	if (recv(connect_SOCKET, temp, BUFLEN, 0) < 0){
 		printf("recv() error %d\n", WSAGetLastError());
-		return -1;
+		return FTP_FAIL;
 	}
 	else{
 		fputs(temp, stdout);
@@ -495,16 +357,16 @@ SOCKET pasv(SOCKET &connect_SOCKET, SOCKET &DataSocket, char* host){
 	{
 		printf("\n[!]Fail connect to server \n");
 		closesocket(DataSocket);
-		return -1;
+		return FTP_FAIL;
 	}
 
-	return DataSocket;
+	return FTP_WIN;
 }
 
 int ls(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct){
 	SOCKET DataSocket;
 	
-	if (cmdstruct->pasv_mode && pasv(connect_SOCKET,DataSocket, host) != -1){
+	if (cmdstruct->pasv_mode && pasv(connect_SOCKET,DataSocket, host) != FTP_FAIL){
 		char retrive[BUFLEN];
 		char f_ls_list[BUFLEN * 5];
 
@@ -513,16 +375,15 @@ int ls(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct){
 
 		if (status < 0){
 			printf("send() error %d\n", WSAGetLastError());
-			return -1;
+			closesocket(DataSocket);
+			return FTP_FAIL;
 		}
 		//get feedback
-		char feedb[BUFLEN];
-		status = feedback(connect_SOCKET, feedb, BUFLEN);
-		if (status < 0){
-			return -1;
-		}
-		else{
-			fputs(feedb, stdout);
+		vector<int> arr_expect_pass = { 150 };
+		status = recv_reply(connect_SOCKET, arr_expect_pass);
+		if (status == FTP_FAIL){
+			closesocket(DataSocket);
+			return FTP_FAIL;
 		}
 
 		//get list
@@ -533,36 +394,40 @@ int ls(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct){
 			strcat(f_ls_list, retrive);
 			status = recv(DataSocket, retrive, BUFLEN, 0);
 		}
-		get226_Successfully_transferred(connect_SOCKET, feedb);
-		fputs(f_ls_list, stdout);
+		//get226_Successfully_transferred
+		arr_expect_pass = { 226 };
+		status = recv_reply(connect_SOCKET, arr_expect_pass);
+		if (status == FTP_FAIL){
+			closesocket(DataSocket);
+			return FTP_FAIL;
+		}
 
+		fputs(f_ls_list, stdout);
 		closesocket(DataSocket);
-		return 0;
 	}
+	return FTP_WIN;
 }
 
-void pwd(SOCKET &connect_SOCKET,char *pwd){
+int pwd(SOCKET &connect_SOCKET,char *pwd){
 	char buff[BUFLEN];
 
 	if (send(connect_SOCKET, pwd, strlen(pwd), 0) < 0){
 		printf("send() error %d\n", WSAGetLastError());
-		return;
+		return FTP_FAIL;
 	}
 	memset(buff, '\0', BUFLEN);
 
 	if (recv(connect_SOCKET, buff, BUFLEN, 0) < 0){
 		printf("recv() error %d\n", WSAGetLastError());
-		return;
+		return FTP_FAIL;
 	}
 	else{
-		int code;
 		fputs(buff, stdout);
-		sprintf(buff, "%d", &code);
-		print_reply_code(code);
 	}
+	return FTP_WIN;
 }
 
-void TYPE_I(SOCKET &connect_SOCKET, char *typei){
+void TYPE(SOCKET &connect_SOCKET, char *typei){
 	char buff[BUFLEN];
 
 	if (send(connect_SOCKET, typei, strlen(typei), 0) < 0){
@@ -576,93 +441,92 @@ void TYPE_I(SOCKET &connect_SOCKET, char *typei){
 		return;
 	}
 	else{
-		int code;
 		fputs(buff, stdout);
-		sprintf(buff, "%d", &code);
-		print_reply_code(code);
 	}
 }
 
 int put(SOCKET &connect_SOCKET, char* host,char *_cmd,command* cmdstruct){
 	SOCKET DataSocket;
-	if (cmdstruct->pasv_mode && pasv(connect_SOCKET, DataSocket, host) != -1){
+	if (cmdstruct->pasv_mode && pasv(connect_SOCKET, DataSocket, host) != FTP_FAIL){
 		//Check whether file is valid?
 
-		FILE* file_out = fopen(cmdstruct->_args, "r");
+		FILE* file_out = fopen(cmdstruct->_args, "rb");
 		if (file_out == NULL){
 			printf("%s is invalid", cmdstruct->_args);
-			return -1;
+			closesocket(DataSocket);
+			return FTP_FAIL;
 		}
 		//send STOR command
 		int status = send(connect_SOCKET, _cmd, strlen(_cmd), 0);
 
 		if (status < 0){
 			printf("send() error %d\n", WSAGetLastError());
-			return -1;
+			closesocket(DataSocket);
+			return FTP_FAIL;
 		}
 		//get feedback
-		char feedb[BUFLEN];
-		status = feedback(connect_SOCKET, feedb, BUFLEN);
+		vector<int> arr_expect_pass = { 150 };
+		status = recv_reply(connect_SOCKET, arr_expect_pass);
+		if (status == FTP_FAIL){
+			closesocket(DataSocket);
+			return FTP_FAIL;
+		}
 
-		if (status < 0){
-			return -1;
-		}
-		else{
-			fputs(feedb, stdout);
-		}
 		//upload file
 		int len;
 		char buf[BUFLEN];
 		memset(buf, '\0', BUFLEN);
-		while ((len = fread(buf, 1, BUFLEN, file_out)) > 0){
+
+		while ((len = fread(buf, sizeof(char), BUFLEN, file_out)) > 0){
 			status = send(DataSocket, buf, len, 0);
 			if (status < 0){
 				printf("send() error %d\n", WSAGetLastError());
-				return -1;
+				closesocket(DataSocket);
+				return FTP_FAIL;
 			}
 		}
 		
 		fclose(file_out);
 		closesocket(DataSocket);
 
-		get226_Successfully_transferred(connect_SOCKET, feedb);
-		return 0;
+		//get226_Successfully_transferred
+		arr_expect_pass = { 226 };
+		status = recv_reply(connect_SOCKET, arr_expect_pass);
+		if (status == FTP_FAIL){
+			return FTP_FAIL;
+		}
+
+		return FTP_WIN;
 	}
-	return -1;
+	return FTP_FAIL;
 }
 
 int get(SOCKET &connect_SOCKET, char* host,char *_cmd, command* cmdstruct){
 	SOCKET DataSocket;
-	if (cmdstruct->pasv_mode && pasv(connect_SOCKET, DataSocket, host) != -1){
+	
+	if (cmdstruct->pasv_mode && pasv(connect_SOCKET, DataSocket, host) != FTP_FAIL){
 		//send RETR command
 		int status = send(connect_SOCKET, _cmd, strlen(_cmd), 0);
 
 		if (status < 0){
 			printf("send() error %d\n", WSAGetLastError());
-			return -1;
+			closesocket(DataSocket);
+			return FTP_FAIL;
 		}
 		//get feedback
-		char feedb[BUFLEN];
-
-		status = feedback(connect_SOCKET, feedb, BUFLEN);
-
-		if (status < 0){
-			return -1;
-		}
-		else{
-			int code;
-			fputs(feedb, stdout);
-			sscanf(feedb, "%d", &code);
-			if (code == 550){
-				return -1;
-			}
+		vector<int> arr_expect_pass = { 150 };
+		status = recv_reply(connect_SOCKET, arr_expect_pass);
+		if (status == FTP_FAIL){
+			closesocket(DataSocket);
+			return FTP_FAIL;
 		}
 
 		//create a new file
-		FILE* file_in = fopen(cmdstruct->_args, "w");
+		FILE* file_in = fopen(cmdstruct->_args, "wb");
 		if (file_in == NULL){
 			printf("Can not get file %s", cmdstruct->_args);
-			return -1;
+			closesocket(DataSocket);
+			return FTP_FAIL;
 		}
 		//download file
 		int len;
@@ -670,44 +534,23 @@ int get(SOCKET &connect_SOCKET, char* host,char *_cmd, command* cmdstruct){
 		memset(buf, '\0', BUFLEN);
 
 		while (len = recv(DataSocket, buf, BUFLEN, 0) > 0){
-			fwrite(buf, 1, BUFLEN, file_in);
+			fwrite(buf, sizeof(char), sizeof(buf), file_in);
 		}
-		get226_Successfully_transferred(connect_SOCKET, feedb);
-		fclose(file_in);
 
+		//get226_Successfully_transferred
+		arr_expect_pass = { 226 };
+		status = recv_reply(connect_SOCKET, arr_expect_pass);
+		if (status == FTP_FAIL){
+			closesocket(DataSocket);
+			return FTP_FAIL;
+		}
+
+		fclose(file_in);
 		closesocket(DataSocket);
 
-		return 0;
+		return FTP_WIN;
 	}
-	return -1;
-}
-
-int get226_Successfully_transferred(SOCKET &connect_SOCKET, char* pre_feedback){
-	int code_226=0;
-	//if pre_feedback has 226 code
-	char* temp=NULL;
-	temp = strtok(pre_feedback, "\n");
-	while (temp!=NULL)
-	{
-		sscanf(temp, "%d", &code_226);
-		temp = strtok(NULL, "\n");
-		if (code_226 == 226){
-			return 226;
-		}
-	}
-	//get 226 from server
-	int status;
-	char feedb[BUFLEN];
-	status = feedback(connect_SOCKET, feedb, BUFLEN);
-	while (code_226 != 226 && status != -1)
-	{
-		sscanf(feedb, "%d", &code_226);
-		if (code_226 == 226){
-			return 226;
-		}
-		status = feedback(connect_SOCKET, feedb, BUFLEN);
-	}
-	return -1;
+	return FTP_FAIL;
 }
 
 int mget(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct){
@@ -740,8 +583,8 @@ int mget(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct){
 	strcat(_cmd1, "\r\n\0");
 
 	while (count > 0){
-		if (get(connect_SOCKET, host, _cmd1, cmdstruct1) == -1){
-			return -1;
+		if (get(connect_SOCKET, host, _cmd1, cmdstruct1) == FTP_FAIL){
+			return FTP_FAIL;
 		}
 		--count;
 		//If still have files need to be downloaded
@@ -759,7 +602,7 @@ int mget(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct){
 	}
 
 	delete cmdstruct1;
-	return 0;
+	return FTP_WIN;
 }
 
 int mput(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct){
@@ -792,7 +635,7 @@ int mput(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct){
 	strcat(_cmd1, "\r\n\0");
 
 	while (count > 0){
-		if (put(connect_SOCKET, host, _cmd1, cmdstruct1) == -1){
+		if (put(connect_SOCKET, host, _cmd1, cmdstruct1) == FTP_FAIL){
 			return -1;
 		}
 		--count;
@@ -811,14 +654,12 @@ int mput(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct){
 	}
 
 	delete cmdstruct1;
-	return 0;
+	return FTP_WIN;
 }
 
 int handle_ftp_exit(SOCKET connect_SOCKET, char * quit_cmd)
 {
 	char buf[BUFLEN];
-	int len_temp;
-	int reply_code;
 	int status;
 
 	// QUIT <CRLF>
@@ -826,23 +667,64 @@ int handle_ftp_exit(SOCKET connect_SOCKET, char * quit_cmd)
 
 	if (status == SOCKET_ERROR) {
 		printf("send() error %d\n", WSAGetLastError());
+		return FTP_FAIL;
 	}
 
 	memset(buf, '\0', BUFLEN);
-	// Check server recieving quit msg
-	while (len_temp = recv(connect_SOCKET, buf, BUFLEN, 0) > 0) {
-		char *p_end = NULL;
-		reply_code = strtol(buf, &p_end, 10);
-		print_reply_code(reply_code);
 
-		// 221 Service closing control connection.
-		//     Logged out if appropriate.
-		if (reply_code == 221) {
-			break;
-		}
-		// error happen
-		return -1;
+	//get feedback
+	vector<int> arr_expect_pass = { 221 };
+	status = recv_reply(connect_SOCKET, arr_expect_pass);
+
+	return status;
+}
+
+int mkdir(SOCKET &connect_SOCKET, char *_cmd){
+	if (send(connect_SOCKET, _cmd, strlen(_cmd), 0) < 0){
+		printf("send() error %d\n", WSAGetLastError());
+		return FTP_FAIL;
 	}
 
-	return 0;
+	vector<int> arr_expect_pass = { 257 };
+	//status may be equal FTP_FAIL or FTP_WIN
+	int status = recv_reply(connect_SOCKET, arr_expect_pass);
+	return status;
+}
+
+int rmdir(SOCKET &connect_SOCKET, char *_cmd){
+	if (send(connect_SOCKET, _cmd, strlen(_cmd), 0) < 0){
+		printf("send() error %d\n", WSAGetLastError());
+		return FTP_FAIL;
+	}
+
+	vector<int> arr_expect_pass = { 250 };
+	//status may be equal FTP_FAIL or FTP_WIN
+	int status = recv_reply(connect_SOCKET, arr_expect_pass);
+	return status;
+}
+
+int exists_in_arr(vector<int> arr, int k)
+{
+	for (size_t i = 0; i < arr.size(); ++i) {
+		if (arr[i] == k)
+			return FTP_WIN;
+	}
+	return FTP_FAIL;
+}
+
+int recv_reply(SOCKET connect_SOCKET, vector<int> arr_expect)
+{
+	char buf[BUFLEN];
+	int len_temp;
+	if ((len_temp = recv(connect_SOCKET, buf, BUFLEN, 0)) > 0) {
+		char *p_end = NULL;
+		buf[len_temp] = '\0';
+		printf("%s", buf);
+		int reply_code = strtol(buf, &p_end, 10);
+
+		if (exists_in_arr(arr_expect, reply_code)== FTP_FAIL) {
+			return FTP_FAIL;
+		}
+	}
+	return FTP_WIN;
 }
--- ftp_client-master/ftp_client/client.h
+++ ftp_client-master/ftp_client/client.h
@@ -1,6 +1,8 @@
 #ifndef _CLIENT_H
 #define _CLIENT_H
 
+#include <conio.h>
+#include <vector>
 #include <stdio.h>
 #include <stdlib.h>
 #include <winsock2.h>
@@ -11,10 +13,13 @@
 
 
 #pragma comment(lib, "Ws2_32.lib")
-
+using namespace std;
 
 #define FTP_PORT "21"
 #define BUFLEN 256
+#define FTP_WIN 0
+#define FTP_FAIL -1
+#define Enter 13
 
 /* _cmd store command such as CWD, LIST,... in server
    _args store arguments such as /home/task /home/bt
@@ -31,36 +36,88 @@ struct command{
 */
 int ftp_login(SOCKET &connect_SOCKET);
 
-/* Print full message from server
-* by reply_code, follow RFC 959
-*/
-void print_reply_code(int reply_code);
 
-/* Read client command and tranfer to server command
-	return 0 if success, return -1 if client command is invalid
+/* Read client command and tranfer to server command.
+	return FTP_WIN if success, return FTP_FAIL if client command is invalid.
 */
 int read_ftpclien_cmd(char* buff, command *cmdstruct);
 
 SOCKET pasv(SOCKET &connect_SOCKET, SOCKET &DataSocket, char* host);
 
+/*
+	This command causes a directory listing to be sent from server to user site.
+	return FTP_WIN if success, return FTP_FAIL if any error.
+*/
 int ls(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct);
 
-void pwd(SOCKET &connect_SOCKET, char *pwd);
+/*
+	This command causes the name of the current working directory to be returned in the reply.
+	return FTP_WIN if success, return FTP_FAIL if any error.
+*/
+int pwd(SOCKET &connect_SOCKET, char *pwd);
 
-void TYPE_I(SOCKET &connect_SOCKET, char *typei);
+/*
+	This command tells the server that the transfer is going to involve a file with 
+	a binary data type.
+	Image mode and Binary mode mean the same thing in FTP.
+*/
+void TYPE(SOCKET &connect_SOCKET, char *typei);
 
+/*	
+	This command causes the server-DTP to accept the data transferred via the data connection 
+	and to store the data as a file at the server site.
+	return FTP_WIN if success, return FTP_FAIL if any error.
+*/
 int put(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct);
 
+/*
+	This command causes the server-[Data Tranfer Process] to transfer a copy of the file, specified in the pathname, 
+	to user-DTP at the other end of the data connection.
+	return FTP_WIN if success, return FTP_FAIL if any error.
+*/
 int get(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct);
 
-int feedback(SOCKET &connect_SOCKET, char* feedback, int size);
-
-int get226_Successfully_transferred(SOCKET &connect_SOCKET, char* pre_feedback);
-
+/*
+	This command causes the server-[Data Tranfer Process] to transfer multi copies of the files,
+	to user-DTP at the other end of the data connection.
+	return FTP_WIN if success, return FTP_FAIL if any error.
+*/
 int mget(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct);
 
+/*
+	This command causes the server-DTP to accept the data transferred via the data connection
+	and to store the data as multi files at the server site.
+	return FTP_WIN if success, return FTP_FAIL if any error.
+*/
 int mput(SOCKET &connect_SOCKET, char* host, char *_cmd, command* cmdstruct);
 
+/*
+	This command causes the client-DTP to logout of server.
+	return FTP_WIN if success, return FTP_FAIL if any error.
+*/
 int handle_ftp_exit(SOCKET connect_SOCKET, char * quit_cmd);
 
+/*
+	This command causes the directory specified in the pathname to be created as a .
+	return FTP_WIN if success, return FTP_FAIL if any error.
+*/
+int mkdir(SOCKET &connect_SOCKET, char *_cmd);
+
+/*
+	This command causes the directory specified in the pathname to be removed as a directory.
+	return FTP_WIN if success, return FTP_FAIL if any error.
+*/
+int rmdir(SOCKET &connect_SOCKET, char *_cmd);
+
+/* 
+	Check if k in arr
+	return FTP_WIN if success, return FTP_FAIL if any error.
+*/
+int exists_in_arr(vector<int> arr, int k);
+
+/* 
+	Receive reply from server
+	return FTP_WIN if success, return FTP_FAIL if any error.
+*/
+int recv_reply(SOCKET connect_SOCKET, vector<int> arr_expect);
 #endif
